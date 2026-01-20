d4f7167cabf837080c0ea48ac9ec9b2ef3dd40a7
jayceyao <jayceyao@tencent.com>
2026-01-20 17:39:52 +0800

    feat: 配置模块v0.1

---
+++ config/config.go
@@ -0,0 +1,68 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	configKey = "workflow"
+)
+
+var (
+	globalPath      string //global configure file path
+	globalConfigure map[string]configuration
+)
+
+type transition struct {
+	FromEvent string `yaml:"from_event"`
+	ToEvent   string `yaml:"to_event"`
+	Expr      string `yaml:"expr"`
+}
+
+type configuration struct {
+	FlowName    string       `yaml:"flow_name"`
+	EventsName  []string     `yaml:"events_name"`
+	Transitions []transition `yaml:"transitions"`
+}
+
+func SetConfigPath(path string) {
+	globalPath = path
+}
+
+func GetConfigPath() string {
+	return globalPath
+}
+
+func parseConfigFile() {
+	fileInfo, err := os.Stat(globalPath)
+	if err != nil {
+		panic(err)
+	}
+	if fileInfo.IsDir() {
+		panic("xxx")
+	}
+
+	file, err := os.Open(globalPath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var configData map[string]interface{}
+	decoder := yaml.NewDecoder(file)
+	if err := decoder.Decode(&configData); err != nil {
+		panic(err)
+	}
+	if workflowConfig, exists := configData[configKey]; exists {
+		workflowData, err := yaml.Marshal(workflowConfig)
+		if err != nil {
+			panic(err)
+		}
+
+		if err := yaml.Unmarshal(workflowData, &globalConfigure); err != nil {
+			panic(err)
+		}
+	}
+}
---
+++ config/config_test.go
@@ -0,0 +1,37 @@
+package config
+
+import "testing"
+
+func Test_parseConfigFile(t *testing.T) {
+	tests := []struct {
+		name     string
+		fileName string
+	}{
+		{
+			name:     "test",
+			fileName: "config.test.yaml",
+		},
+	}
+	SetConfigPath("./config.test.yaml")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			SetConfigPath(tt.fileName)
+			parseConfigFile()
+			config := globalConfigure
+			if testFlow, existed := config["test_flow"]; !existed {
+				t.Errorf("test_flow not existed")
+			} else {
+				if testFlow.FlowName != "test_flow" {
+					t.Errorf("test_flow name not match")
+				}
+				if len(testFlow.EventsName) != 4 {
+					t.Errorf("test_flow events not match")
+				}
+				if len(testFlow.Transitions) != 3 {
+					t.Errorf("test_flow transitions not match")
+				}
+			}
+
+		})
+	}
+}
