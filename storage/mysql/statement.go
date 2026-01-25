162f33e8b5e787f4647762bf56dc6a79e3c9bb27
jayceyao <417077351@qq.com>
2026-01-25 23:13:30 +0800

    feat: 添加flow表

--- storage/mysql/statement.go
+++ storage/mysql/statement.go
@@ -3,3 +3,13 @@ package mysql
 const (
 	CreateEvent = "INSERT INTO event_queue(`event_id`,`type`,`name`,`status`,`flow_id`,`flow_type`)VALUES(?,?,?,?,?,?)"
 )
+
+const (
+	create_flow_table = `
+CREATE TABLE work_flow(
+    flow_id varchar(256),
+    flow_type varchar(256),
+    flow_name varchar(256),
+    current_event varchar(256)
+)`
+)
